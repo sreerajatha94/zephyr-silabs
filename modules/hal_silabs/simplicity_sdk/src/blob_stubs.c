85ce909294554411c8157fd48571dae4a9ce02bf
sree sreerajatha <sree.sreerajatha@silabs.com>
2026-03-04 09:55:39 -0500

    tests: drivers: counter: Add counter driver test
    Add counter driver test so that it can build with BUILD_ONLY_NO_BLOBS
    and successfully use RAIL stubs defined in blob_stub.c
    
    Signed-off-by: sree sreerajatha <sree.sreerajatha@silabs.com>

--- modules/hal_silabs/simplicity_sdk/src/blob_stubs.c
+++ modules/hal_silabs/simplicity_sdk/src/blob_stubs.c
@@ -11,6 +11,7 @@
 
 #include <rail.h>
 #include <sl_status.h>
+#include <sl_rail.h>
 
 RAIL_Status_t RAIL_VerifyTxPowerCurves(const struct RAIL_TxPowerCurvesConfigAlt *config)
 {
@@ -101,4 +102,32 @@ void HOSTMAILBOX_IRQHandler(void)
 
 void RDMAILBOX_IRQHandler(void)
 {
+}
+
+/* RAIL timer API stubs (e.g. for counter_silabs_protimer when no blobs) */
+sl_rail_time_t sl_rail_get_time(sl_rail_handle_t rail_handle)
+{
+	return 0U;
+}
+
+sl_rail_status_t sl_rail_set_multi_timer(sl_rail_handle_t rail_handle,
+					 sl_rail_multi_timer_t *timer,
+					 sl_rail_time_t expiration_time,
+					 sl_rail_time_mode_t mode,
+					 sl_rail_multi_timer_callback_t callback,
+					 void *user_data)
+{
+	return SL_RAIL_STATUS_NO_ERROR;
+}
+
+sl_rail_status_t sl_rail_cancel_multi_timer(sl_rail_handle_t rail_handle,
+					    sl_rail_multi_timer_t *timer)
+{
+	return SL_RAIL_STATUS_NO_ERROR;
+}
+
+bool sl_rail_is_multi_timer_running(sl_rail_handle_t rail_handle,
+				    sl_rail_multi_timer_t *timer)
+{
+	return false;
 }
