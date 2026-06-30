b862b2f309223dc887ffb4c41579f9062454737e
sree sreerajatha <sree.sreerajatha@silabs.com>
2026-06-30 13:38:13 -0400

    Add zigbee library support and sample app
    Added CMakeList and KConfigs for board support
    
    Signed-off-by: sree sreerajatha <sree.sreerajatha@silabs.com>

---
+++ modules/hal_silabs/simplicity_sdk/config/zigbee/sl_zigbee_config.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026 Silicon Laboratories Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Zephyr Kconfig to Silicon Labs Zigbee configuration mapping.
+ * Extend this header as Zigbee SDK sources and runtime glue are integrated.
+ */
+
+#ifndef SL_ZIGBEE_CONFIG_H
+#define SL_ZIGBEE_CONFIG_H
+
+#endif /* SL_ZIGBEE_CONFIG_H */
